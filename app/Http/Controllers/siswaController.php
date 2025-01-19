a5a97ec7279f3ba6333c129d1ec51f3bf8fd3b09
Shafwan <arul.safwan73@gmail.com>
2025-01-19 09:56:35 +0800

    feat: Implement siswa edit view and update index for improved navigation and functionality

--- app/Http/Controllers/siswaController.php
+++ app/Http/Controllers/siswaController.php
@@ -43,7 +43,7 @@ class siswaController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view('main.admin.siswa.edit');
     }
 
     /**
--- resources/views/main/admin/siswa/index.blade.php
+++ resources/views/main/admin/siswa/index.blade.php
@@ -33,7 +33,8 @@
                                             <td>RPL 2</td>
                                             <td>0/1</td>
                                             <td>
-                                                <a href="{{ route('siswa.show', $i) }}" class="avatar bg-primary text-white avatar-sm me-2">
+                                                <a href="{{ route('siswa.show', $i) }}"
+                                                    class="avatar bg-primary text-white avatar-sm me-2">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24"
                                                         height="24" viewBox="0 0 24 24" fill="none"
                                                         stroke="currentColor" stroke-width="1" stroke-linecap="round"
@@ -45,7 +46,8 @@
                                                             d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                                     </svg>
                                                 </a>
-                                                <a href="#" class="avatar bg-yellow text-white avatar-sm me-2">
+                                                <a href="{{ route('siswa.edit', $i) }}"
+                                                    class="avatar bg-yellow text-white avatar-sm me-2">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24"
                                                         height="24" viewBox="0 0 24 24" fill="none"
                                                         stroke="currentColor" stroke-width="1" stroke-linecap="round"
@@ -59,7 +61,8 @@
                                                         <path d="M16 5l3 3" />
                                                     </svg>
                                                 </a>
-                                                <a href="#" class="avatar bg-danger text-white avatar-sm">
+                                                <button class="avatar bg-danger text-white avatar-sm border-0" type="button"
+                                                    data-bs-toggle="modal" data-bs-target="#exampleModal">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24"
                                                         height="24" viewBox="0 0 24 24" fill="none"
                                                         stroke="currentColor" stroke-width="1" stroke-linecap="round"
@@ -72,7 +75,7 @@
                                                         <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
                                                         <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
                                                     </svg>
-                                                </a>
+                                                </button>
                                             </td>
                                         </tr>
                                     @endfor
@@ -86,4 +89,40 @@
     </div>
 </div>
 
+<div class="modal" id="exampleModal" tabindex="-1">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-status bg-danger"></div>
+            <div class="modal-body text-center py-4">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24"
+                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 9v2m0 4v.01" />
+                    <path
+                        d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75" />
+                </svg>
+                <h3>Anda Yakin?</h3>
+                <div class="text-secondary">Yakin untuk menghapus data Siswa ini?</div>
+            </div>
+            <div class="modal-footer">
+                <div class="w-100">
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="btn w-100" data-bs-dismiss="modal">
+                                Batal
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="btn btn-danger w-100" data-bs-dismiss="modal">
+                                Hapus Data
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @include('layouts.footerLayout')
